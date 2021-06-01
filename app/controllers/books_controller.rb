39d81f0900dbaf72e67d98d5106d17c680e87ad0
kinoshitaken123 <k1k7s9e3@gmail.com>
2021-06-01 14:30:46 +0000

    [Update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,8 @@
 class BooksController < ApplicationController
-
+  before_action :authenticate_user!
   def show
     @book = Book.find(params[:id])
-    @user = @book.user
+    @book_new = Book.new
   end
 
   def index
@@ -49,6 +49,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+  
+  def ensure_correct_user
+      @book = Book.find(params[:id])
+    unless @book.user == current_user
+        redirect_to books_path
+    end
   end
 
 end
